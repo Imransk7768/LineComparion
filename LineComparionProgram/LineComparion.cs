9ca703d2091540012851b70b60e075710af4dfef
imransk7768 <imransk7768@gmail.com>
2022-04-09 00:13:48 +0530

    [Imran] Add : Welcome to lineComparion

---
+++ LineComparionProgram/LineComparion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparionProgram
+{
+    public class LineComparion
+    {
+        public void Welcome()
+        {
+            Console.WriteLine("Welcome to Line Comparion");
+        }
+    }
+}
