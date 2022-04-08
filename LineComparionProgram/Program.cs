6d3ed58f7f28297a0acf5f8aac288b013bf48750
imransk7768 <imransk7768@gmail.com>
2022-04-09 00:58:16 +0530

    [Imran] Add : Check Greater Line Length

--- LineComparionProgram/Program.cs
+++ LineComparionProgram/Program.cs
@@ -6,17 +6,22 @@ namespace LineComparionProgram
         public static void Main(String[] args)
         {
 
-            LineComparion line1 = new LineComparion(2, 5, 6, 8);
-            double l1 = line1.calculateLength();
-            LineComparion line2 = new LineComparion(2, 5, 6, 8);
-            double l2 = line2.calculateLength();
-            if (l1.CompareTo(l2)==0)
+            LineComparion Line1 = new LineComparion(2, 5, 7, 8);
+            double l1 = Line1.CalculateLength();
+            LineComparion Line2 = new LineComparion(7, 8, 9, 9);
+            double l2 = Line2.CalculateLength();
+            if (l1.CompareTo(l2) == 0)
             {
-                Console.WriteLine("Both Lines are equals ");
+                Console.WriteLine("Both Lengths Are Equal.");
+            }
+            if (l1.CompareTo(l2) > 0)
+            {
+                Console.WriteLine(" Line-1 Is Greater.");
             }
             else
             {
-                Console.WriteLine("Both Lines are not equals ");
+                Console.WriteLine(" Line-2 Is Greater.");
+
             }
         }
     }
